549958cd10e77ac5f77540833d2c4c52c1cf5ad5
abrahamj123 <100929072+abrahamj123@users.noreply.github.com>
2022-03-05 13:16:49 +0530

    Create Sunlight .sql

---
+++ Sunlight .sql
@@ -0,0 +1,9 @@
+WHat is average sunlight kw threshod  recieved in Florida  ?
+
+
+Select state_name,(yearly_sunlight_kwh_kw_threshold_avg)
+FROM `bigquery-public-data.sunroof_solar.solar_potential_by_postal_code`
+where state_name ='Florida' 
+ 
+
+ 
